d58a034cde7dfb39aa7fdfc2fcf250fa3f921e6a
Putra Dwi Nanda Prijambodo <putradwinandaprijambodo@hotmail.com>
2025-08-19 21:06:36 +0700

    feat(seed): seed product

---
+++ apps/api/database/seeders/ProductSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('products')->updateOrInsert(
+            ['id' => 1],
+            [
+                'name'        => 'Product 1',
+                'description' => 'Description for product 1',
+                'price'      => 10,
+                'price_currency' => 'USD',
+                'has_license' => false,
+            ]
+        );
+    }
+}
