c58bc14c1b2b3dc83810d5f2057d6af5bbb39a2c
Putra Dwi Nanda Prijambodo <putradwinandaprijambodo@hotmail.com>
2025-08-23 13:17:06 +0700

    feat(auth): add login route with LoginController

--- apps/api/routes/web.php
+++ apps/api/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')
+    ->middleware(['web']) // penting untuk cookie/session
+    ->group(function () {
+        Route::post('/login', [LoginController::class, 'authenticate']);
+    });
 
 Route::get('/', function () {
     return view('welcome');
