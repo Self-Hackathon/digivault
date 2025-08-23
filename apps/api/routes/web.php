156bfb22558cdb2714b75bb691f3263c2ab68034
Putra Dwi Nanda Prijambodo <putradwinandaprijambodo@hotmail.com>
2025-08-23 15:23:24 +0700

    feat(routes): update auth login to AuthController and add products index route

--- apps/api/routes/web.php
+++ apps/api/routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
-use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')
     ->middleware(['web']) // penting untuk cookie/session
     ->group(function () {
-        Route::post('/login', [LoginController::class, 'authenticate']);
+        Route::post('/login', [AuthController::class, 'login']);
     });
+
+Route::get('/products', [ProductsController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
