bafd6ef0cacf22947c87c22fc6d49c7728da53ca
Putra Dwi Nanda Prijambodo <putradwinandaprijambodo@hotmail.com>
2025-08-23 15:22:07 +0700

    refactor(model): update Product to extend Eloquent Model

--- apps/api/app/Models/Product.php
+++ apps/api/app/Models/Product.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class Product
+class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory, Notifiable;
