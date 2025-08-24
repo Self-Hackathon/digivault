2d5384b7c40112b2e69b9fa4327fa76de802c7d8
Putra Dwi Nanda Prijambodo <putradwinandaprijambodo@hotmail.com>
2025-08-24 15:13:08 +0700

    feat(ui): add Components shadcn

---
+++ apps/web/components/admin/auth/login-form.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { AlertCircleIcon } from "lucide-react"
+
+import { Alert, AlertTitle } from "@/components/ui/alert"
+
+import { AxiosError } from "axios"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { authApi } from "@/lib/api";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (loading) return;
+
+    setLoading(true)
+    setError(null)
+
+    const formData = new FormData(e.currentTarget)
+    const email = formData.get("email") as string
+    const password = formData.get("password") as string
+
+    try {
+      await authApi.authLoginPost({
+        loginRequest: { email, password },
+      })
+      router.push("/admin");
+    } catch (err: unknown) {
+      if (err instanceof AxiosError) {
+        const apiMessage = err.response?.data?.error
+        setError(apiMessage ?? "Login failed")
+        console.error("Auth error:", apiMessage)
+      } else {
+        console.error("Unexpected error:", err)
+        setError("Unexpected error occurred")
+      }
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" name="password" type="password" required />
+              </div>
+              {error && (
+                <Alert variant="destructive">
+                  <AlertCircleIcon />
+                  <AlertTitle>
+                    {error}
+                  </AlertTitle>
+                </Alert>
+              )}
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full" disabled={loading}>
+                  {loading ? "Logging in..." : "Login"}
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
