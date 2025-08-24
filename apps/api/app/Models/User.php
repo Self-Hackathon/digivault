707dae55eb8f98678369bac47b01907ba4216181
Putra Dwi Nanda Prijambodo <putradwinandaprijambodo@hotmail.com>
2025-08-24 21:44:02 +0700

    feat(user): add role & permissions accesors with UserRole relation

--- apps/api/app/Models/User.php
+++ apps/api/app/Models/User.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\hasOneThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
+
+    protected $with = [];
 
     /**
      * The attributes that are mass assignable.
@@ -30,6 +34,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
+        'roles'
     ];
 
     /**
@@ -44,4 +49,43 @@ class User extends Authenticatable
             'is_active' => 'boolean',
         ];
     }
+
+    protected $appends = ['role', 'permissions'];
+
+    /**
+     * Summary of roles
+     * @return hasOneThrough<Role, UserRole, User>
+     */
+    public function roles(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            Role::class,
+            UserRole::class,
+            'user_id',
+            'id',
+            'id',
+            'role_id'
+        );
+    }
+
+    /**
+     * Get the users role name.
+     *
+     * @return string|null
+     */
+    public function getRoleAttribute(): ?string
+    {
+        return $this->roles?->name;
+    }
+
+    /**
+     * Get the users permissions.
+     *
+     * @return array
+     */
+    public function getPermissionsAttribute(): array
+    {
+        return $this->roles?->permissions->pluck('name')->toArray() ?? [];
+    }
+
 }
